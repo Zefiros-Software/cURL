7d0610431824daf2df8bc0b1e1539c34d5cba8cd
PaulVisscher <paul.pev@hotmail.com>
2016-09-08 17:41:58 +0200

    Absolute path

--- .build.1.lua
+++ .build.1.lua
@@ -18,7 +18,7 @@ project "cURL"
 
     local downloadPem = path.join(zpm.build._currentExportPath, "cacert.pem")
     postbuildcommands { 
-        "{COPY} ".. downloadPem .." %{cfg.targetdir.abspath}/cacert.pem" 
+        "{COPY} ".. downloadPem .." %{path.getabsolute(cfg.targetdir)}/cacert.pem" 
     }
 
     zpm.export(function()
