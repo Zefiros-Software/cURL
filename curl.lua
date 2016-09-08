db3047f49fe801b279c42fa920bc9d02188d6f4b
PaulVisscher <paul.pev@hotmail.com>
2016-09-08 13:42:36 +0200

    Copy when needed

--- curl.lua
+++ curl.lua
@@ -1,5 +1,4 @@
 
-if os.isfile( zpm.build._currentExportPath .. "/include/curl/curlbuild.h" ) then
-    zpm.assert( os.remove( zpm.build._currentExportPath .. "/include/curl/curlbuild.h" ) )
+if os.isfile( zpm.build._currentExportPath .. "/include/curl/curlbuild.h" ) == false then
+    zpm.assert( os.copyfile( zpm.build._currentExportPath .. "/include/curl/curlbuild.h.dist", zpm.build._currentExportPath .. "/include/curl/curlbuild.h") )
 end
-zpm.assert( os.copyfile( zpm.build._currentExportPath .. "/include/curl/curlbuild.h.dist", zpm.build._currentExportPath .. "/include/curl/curlbuild.h") )
