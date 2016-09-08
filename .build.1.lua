fbdad1db14c03565d7f147bd964c779a0d798688
PaulVisscher <paul.pev@hotmail.com>
2016-09-08 16:16:12 +0200

    local cacert.pem

--- .build.1.lua
+++ .build.1.lua
@@ -31,28 +31,8 @@ project "cURL"
             "CURL_STATICLIB",
             "HTTP_ONLY", 
             "USE_MBEDTLS",
-            "CURL_HIDDEN_SYMBOLS"
-            --"CURLOPT_SSL_VERIFYPEER=0"
+            "CURL_HIDDEN_SYMBOLS",
+            "./cacert.pem"
         }
-        -- find the location of the ca bundle
-        local ca = nil
-        for _, f in ipairs {
-             downloadPem,
-            "/etc/ssl/certs/ca-certificates.crt",
-            "/etc/pki/tls/certs/ca-bundle.crt",
-            "/usr/share/ssl/certs/ca-bundle.crt",
-            "/usr/local/share/certs/ca-root.crt",
-            "/etc/ssl/cert.pem" } do
-            if os.isfile(f) then
-                ca = f
-                break
-            end
-        end
-        if ca then
-        print(ca)
-            defines { 
-                'CURL_CA_BUNDLE="' .. ca .. '"' 
-            }
-        end
 
     end)
