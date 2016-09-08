d8a5d24ec2ef932727d7962fe8a1dcdc185a6fec
PaulVisscher <paul.pev@hotmail.com>
2016-09-08 18:59:33 +0200

    relative cacert.pem

--- .build.1.lua
+++ .build.1.lua
@@ -16,6 +16,8 @@ project "cURL"
         "src/**.c"
     }
 
+    local downloadPem = path.join(zpm.build._currentExportPath, "cacert.pem")
+
     zpm.export(function()
         includedirs { 
             "include"
@@ -28,5 +30,17 @@ project "cURL"
             "USE_MBEDTLS",
             "CURL_HIDDEN_SYMBOLS"
         }
+
+        filter {"system:windows"}
+            defines { 
+                'CURL_CA_BUNDLE="%__APPDIR__%/cacert.pem"'
+            }
+
+        filter {"system:not windows"}
+            defines {
+                'CURL_CA_BUNDLE="/proc/self/exe/cacert.pem"'
+            }
+
+        filter {}
 
     end)
