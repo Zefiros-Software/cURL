de2a81865138dc07ede6068820f144f5506ffebf
PaulVisscher <paul.pev@hotmail.com>
2016-09-08 13:38:29 +0200

    Remove existing build

--- curl.lua
+++ curl.lua
@@ -1,2 +1,5 @@
 
+if os.isfile( zpm.build._currentExportPath .. "/include/curl/curlbuild.h" ) then
+    os.remove( zpm.build._currentExportPath .. "/include/curl/curlbuild.h" )
+end
 zpm.assert( os.copyfile( zpm.build._currentExportPath .. "/include/curl/curlbuild.h.dist", zpm.build._currentExportPath .. "/include/curl/curlbuild.h") )
