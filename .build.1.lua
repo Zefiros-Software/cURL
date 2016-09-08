2fcad7d155c20254494e7a54647bbe88a7d92e38
PaulVisscher <paul.pev@hotmail.com>
2016-09-08 15:17:11 +0200

    Download pem

--- .build.1.lua
+++ .build.1.lua
@@ -12,7 +12,6 @@ project "cURL"
     }
 
     files {
-        "**.h",
         "lib/**.c"
     }
 
@@ -25,30 +24,32 @@ project "cURL"
             "BUILDING_LIBCURL",
             "CURL_STATICLIB",
             "HTTP_ONLY", 
-            "USE_MBEDTLS" 
+            "USE_MBEDTLS",
+            "CURL_HIDDEN_SYMBOLS"
+            --"CURLOPT_SSL_VERIFYPEER=0"
         }
 
-        filter { "system:linux" }
-            defines { "CURL_HIDDEN_SYMBOLS" }
+        defines {  }
 
-            -- find the location of the ca bundle
-            local ca = nil
-            for _, f in ipairs {
-                "/etc/ssl/certs/ca-certificates.crt",
-                "/etc/pki/tls/certs/ca-bundle.crt",
-                "/usr/share/ssl/certs/ca-bundle.crt",
-                "/usr/local/share/certs/ca-root.crt",
-                "/etc/ssl/cert.pem" } do
-                if os.isfile(f) then
-                    ca = f
-                    break
-                end
+        local downloadPem = zpm.build._currentExportPath .. "cacert.pem"
+        -- find the location of the ca bundle
+        local ca = nil
+        for _, f in ipairs {
+            "/etc/ssl/certs/ca-certificates.crt",
+            "/etc/pki/tls/certs/ca-bundle.crt",
+            "/usr/share/ssl/certs/ca-bundle.crt",
+            "/usr/local/share/certs/ca-root.crt",
+            "/etc/ssl/cert.pem",
+             downloadPem } do
+            if os.isfile(f) then
+                ca = f
+                break
             end
-            if ca then
-                defines { 
-                    'CURL_CA_BUNDLE="' .. ca .. '"' 
-                }
-            end
+        end
+        if ca then
+            defines { 
+                'CURL_CA_BUNDLE="' .. ca .. '"' 
+            }
+        end
 
-        filter {}
     end)
--- curl.lua
+++ curl.lua
@@ -1,4 +1,5 @@
 
 if os.isfile( zpm.build._currentExportPath .. "/include/curl/curlbuild.h" ) == false then
     zpm.assert( os.copyfile( zpm.build._currentExportPath .. "/include/curl/curlbuild.h.dist", zpm.build._currentExportPath .. "/include/curl/curlbuild.h") )
+    zpm.util.download( "https://curl.haxx.se/ca/cacert.pem", zpm.build._currentExportPath .. "/cacert.pem" )
 end
