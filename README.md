289984297a6d48523ca21d05feddf7bd5178625c
PaulVisscher <paul.pev@hotmail.com>
2017-01-23 10:55:49 +0100

    Removed unused dependencies

--- README.md
+++ README.md
@@ -16,3 +16,9 @@ In `premake5.lua`
 ```lua
 zpm.uses "Zefiros-Software/cURL"
 ```
+
+# Status
+Type        | Status
+----------- | -------
+Linux & OSX | [![Build Status](https://travis-ci.org/Zefiros-Software/SFML.svg?branch=master)](https://travis-ci.org/Zefiros-Software/SFML)
+Windows     | [![Build status](https://ci.appveyor.com/api/projects/status/b4syumypyox1y09w?svg=true)](https://ci.appveyor.com/project/Zefiros-Software/sfml)
--- script/install-unix.sh
+++ script/install-unix.sh
@@ -8,14 +8,4 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo update-alternatives --config gcc
     sudo update-alternatives --config g++
     sudo apt-get install gcc-${VERSION}-multilib g++-${VERSION}-multilib
-
-    # needed for SFML
-    sudo apt-get install freeglut3-dev 
-    sudo apt-get install libjpeg-dev 
-    sudo apt-get install libfreetype6-dev 
-    sudo apt-get install libxrandr-dev 
-    sudo apt-get install libglew-dev 
-    sudo apt-get install libsndfile1-dev 
-    sudo apt-get install libopenal-dev
-    sudo apt-get install libudev-dev
 fi
