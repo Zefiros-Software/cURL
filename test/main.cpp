2fabf61ec78233bd83485ca742af95dc3bb8472c
p.e.visscher <p.e.visscher@zefiros.eu>
2018-05-31 20:43:19 +0200

    Update ci

--- test/main.cpp
+++ test/main.cpp
@@ -5,11 +5,11 @@
 #include <curl/curl.h>
 
 std::string data;
-size_t writeCallback( char *buf, size_t size, size_t nmemb, void *up )
+size_t writeCallback(char *buf, size_t size, size_t nmemb, void *up)
 {
-    for ( int c = 0; c < size * nmemb; c++ )
+    for (int c = 0; c < size * nmemb; c++)
     {
-        data.push_back( buf[c] );
+        data.push_back(buf[c]);
     }
 
     return size * nmemb;
@@ -19,18 +19,18 @@ int main()
 {
     CURL *curl;
 
-    curl_global_init( CURL_GLOBAL_ALL );
+    curl_global_init(CURL_GLOBAL_ALL);
     curl = curl_easy_init();
 
-    curl_easy_setopt( curl, CURLOPT_URL, "http://www.zefiros.com/" );
-    curl_easy_setopt( curl, CURLOPT_WRITEFUNCTION, &writeCallback );
-    curl_easy_setopt( curl, CURLOPT_VERBOSE, 1L );
+    curl_easy_setopt(curl, CURLOPT_URL, "http://www.zefiros.com/");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &writeCallback);
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
 
-    curl_easy_perform( curl );
+    curl_easy_perform(curl);
 
     std::cout << std::endl << data << std::endl;
 
-    curl_easy_cleanup( curl );
+    curl_easy_cleanup(curl);
     curl_global_cleanup();
 
     return 0;
